9f56debd1549ab2849e328e299b66d427abfa4bd
ivan.taranenko <vanokalipsi@egmail.com>
2022-08-10 14:57:10 +0300

    connect with api & add features:axios & add button with search functional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 function App() {
 
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=068e6c5a4a3a3a4e1bbfcb3fca0b6559';
+
+    // Declare a new state variable, which we'll call "data"
+    const [data, setData] = useState({})
+    // Declare a new state variable, which we'll call "location"
+    const [location, setLocation] = useState('')
 
 
-  return (
-    <div className="App">
-    </div>
-  );
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=068e6c5a4a3a3a4e1bbfcb3fca0b6559`
+
+    // const searchLocation = (event) => {
+    //     if(event.key === 'Enter'){
+    //         axios.get(url).then((response) => {
+    //             setData(response.data)
+    //             console.log(response.data)
+    //         })
+    //         setLocation('')
+    //     }
+    // }
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data)
+            })
+            setLocation('')
+        }
+    }
+
+    return (
+        <div className="app">
+            <div className="search">
+                <input
+                    value={location}
+                    onChange={event => setLocation(event.target.value)}
+                    onKeyPress={searchLocation}
+                    placeholder='Enter Location'
+                    type="text" />
+            </div>
+            <div className="container">
+                <div className="top">
+                    <div className="location">
+                        <p>{data.name}</p>
+                    </div>
+                    <div className="temp">
+                        {data.main ? <h1>{data.main.temp} °F</h1> : null }
+                    </div>
+                    <div className="description">
+                        {data.weather ? <h2>{data.weather['0'].description} </h2> : null }
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="feels">
+                        {data.main ? <p className="bold">{data.main.feels_like}</p>  : null }
+                        <p>Feels Like</p>
+                    </div>
+                    <div className="humidity ">
+                        {data.main ? <p className="bold">{data.main.humidity}</p>  : null }
+                        <p>Humidity</p>
+                    </div>
+                    <div className="wind ">
+                        {data.wind ? <p className="bold">{data.wind.speed}</p>  : null }
+                        <p>Wind Speed</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
